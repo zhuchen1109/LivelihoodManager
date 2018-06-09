61398fdd98a544760b0edfc4027ff608b0e02c57
zhuchen <zc2027208@sina.com>
2018-06-10 00:51:51 +0800

    uwsgi部署配置

--- livelihood/settings.py
+++ livelihood/settings.py
@@ -109,6 +109,7 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = '/usr/local/var/www/livelihood/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
